5f45111699548d3c384da56b4296a3bd536305fd
Herman Yanush <gera@disroot.org>
2025-03-30 09:08:09 +0300

    broken reflections

--- reflect-bullets.cpp
+++ reflect-bullets.cpp
@@ -4,10 +4,61 @@
 
 using namespace std;
 
+#define EPS 1e-6
+
 SDL_Window* window;
 SDL_Renderer* renderer;
 const int FPS = 60;
 SDL_Rect world = {0, 0, 60, 80};
+
+struct Segment
+{
+	float x1, y1, x2, y2;
+};
+
+vector<Segment> mirrors = 
+{
+	// {(float)world.x, (float)world.y, (float)world.w-1, (float)world.y},
+	// {(float)world.w-1, (float)world.y, (float)world.w-1, (float)world.h-1},
+	// {(float)world.x, (float)world.h-1, (float)world.w-1, (float)world.h-1},
+	// {(float)world.x, (float)world.y, (float)world.x, (float)world.h-1},
+	// {0, 30, 30, 30},
+	{25, 0, 25, 10}
+};
+
+float distance(SDL_FPoint p, SDL_FPoint q)
+{
+	return sqrtf((q.x - p.x) * (q.x - p.x) + (q.y - p.y) * (q.y - p.y));
+}
+
+float line_angle(Segment line)
+{
+	return atan2f(line.y2 - line.y1, line.x2 - line.x1);
+}
+
+bool solve2(float a1, float b1, float c1, float a2, float b2, float c2,
+            float& s, float& t)
+{
+	float den = a1 * b2 - b1 * a2;
+	if (fabs(den) < EPS)
+		return false;
+	s = (c1 * b2 - b1 * c2) / den;
+	t = (a1 * c2 - c1 * a2) / den;
+	return true;
+}
+
+bool intersection_point(Segment seg1, Segment seg2, SDL_FPoint& p)
+{
+	float s0, t0;
+	bool result = solve2(seg1.x2 - seg1.x1, seg2.x1 - seg2.x2, seg2.x1 - seg1.x1,
+	                     seg1.y2 - seg1.y1, seg2.y1 - seg2.y2, seg2.y2 - seg1.y1,
+	                     s0, t0);
+	if (!result || s0 < 0.0f || s0 > 1.0f || t0 < 0.0f || t0 > 1.0f)
+		return false;
+	p.x = seg1.x1 + s0 * (seg1.x2 - seg1.x1);
+	p.y = seg1.y1 + s0 * (seg1.y2 - seg1.y1);
+	return true;
+}
 
 struct Trajectory
 {
@@ -15,50 +66,75 @@ struct Trajectory
 	float length;
 	float angle;
 
-	struct Bend
-	{
-		float dist;
-		float new_angle;
-	};
-
-	vector<Bend> bends;
+	vector<SDL_FPoint> points;
 
 	void draw()
 	{
-		calc_bends();
-
-		SDL_FPoint segment_origin = origin;
-		float segment_angle = angle;
-		float segment_dist = 0;
-		float segment_length;
-		SDL_FPoint segment_end;
+		calc_points();
 		
-		for (int i = 0; i < bends.size(); ++i)
+		for (int i = 1; i < points.size(); ++i)
 		{
-			float segment_length = bends[i].dist - segment_dist;
-
-			segment_end = {sin(segment_angle) * segment_length + segment_origin.x,
-			               cos(segment_angle) * segment_length + segment_origin.y};
+			SDL_FPoint segment_start = {points[i-1].x, points[i-1].y};
+			SDL_FPoint segment_end = {points[i].x, points[i].y};
 			SDL_RenderDrawLine(renderer,
-			                   segment_origin.x, segment_origin.y,
+			                   segment_start.x, segment_start.y,
 			                   segment_end.x, segment_end.y);
-
-			segment_dist = bends[i].dist;
-			segment_angle = bends[i].new_angle;
-			segment_origin = segment_end;
+			segment_start = segment_end;
 		}
-
-		segment_length = length - segment_dist;
-		segment_end = {sin(segment_angle) * segment_length + segment_origin.x,
-		               cos(segment_angle) * segment_length + segment_origin.y};
-		SDL_RenderDrawLine(renderer,
-		                   segment_origin.x, segment_origin.y,
-		                   segment_end.x, segment_end.y);
 	}
 
-	void calc_bends()
+	void calc_points()
 	{
-		bends.clear();
+		points.clear();
+		points.push_back({origin.x, origin.y});
+		float remaining_length = length;
+		int n = 0;
+		float direction = angle;
+		int mirror_last = -1;
+		while (true)
+		{
+			Segment ray = {points[n].x, points[n].y,
+			               points[n].x + remaining_length * cosf(direction),
+			               points[n].y + remaining_length * sinf(direction)};
+			points.push_back({ray.x2, ray.y2});
+			++n;
+			int mirror_idx = -1;
+			float mirror_dist;
+			SDL_FPoint reflection_point;
+			for (int i = 0; i < mirrors.size(); ++i)
+			{
+				if (i == mirror_last)
+					continue;
+				SDL_FPoint point;
+				bool result = intersection_point(ray, mirrors[i], point);
+				if (!result)
+				{
+					continue;
+				}
+				float dist = distance({ray.x1, ray.y1}, point);
+				if (mirror_idx < 0 || dist < mirror_dist)
+				{
+					reflection_point = point;
+					mirror_idx = i;
+					mirror_dist = dist;
+				}
+			}
+			if (mirror_idx < 0)
+			{
+				SDL_Log("no intersects");
+				break;
+			}
+			mirror_last = mirror_idx;
+			SDL_Log("reflection off mirror %d at %lf, %lf", mirror_last, reflection_point.x, reflection_point.y);
+			points[n] = reflection_point;
+			ray.x2 = reflection_point.x;
+			ray.y2 = reflection_point.y;
+			float ray_angle = line_angle(ray);
+			float mirror_angle = line_angle(mirrors[mirror_last]);
+			direction = 2 * (mirror_angle - ray_angle) + ray_angle;
+			remaining_length -= mirror_dist;
+		}
+		SDL_Log("%ld\n", points.size());
 	}
 };
 
@@ -80,7 +156,7 @@ void delay_frame(Uint64& cnt_start, Uint64& cnt_end)
 
 int main()
 {
-	Trajectory tr = {{20.0f, 20.0f}, 10.0f, M_PI/4.0f};
+	Trajectory tr = {{20.0f, 20.0f}, 100.0f, -M_PI/4.0f};
 
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer(600, 800, SDL_WINDOW_RESIZABLE, &window, &renderer);
@@ -106,9 +182,13 @@ int main()
 		SDL_RenderFillRect(renderer, &world);
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 		tr.draw();
-		tr.angle += 0.1;
+		// tr.angle += 0.01;
+		for (int i = 0; i < mirrors.size(); ++i)
+		{
+			SDL_SetRenderDrawColor(renderer, 200, 200, 0, 255);
+			SDL_RenderDrawLine(renderer, mirrors[i].x1, mirrors[i].y1, mirrors[i].x2, mirrors[i].y2);
+		}
 		SDL_RenderPresent(renderer);
-
 		cnt_end = SDL_GetPerformanceCounter();
 		delay_frame(cnt_start, cnt_end);
 	}
